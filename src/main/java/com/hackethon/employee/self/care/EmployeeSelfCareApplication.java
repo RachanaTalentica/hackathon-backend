9498e001121bc88c7bed7b6276b63cf0e18af487
RachanaTalentica <rachana.bhagavath@talentica.com>
2023-07-28 23:27:36 +0530

    initial commit

---
+++ src/main/java/com/hackethon/employee/self/care/EmployeeSelfCareApplication.java
@@ -0,0 +1,16 @@
+package com.hackethon.employee.self.care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSelfCareApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSelfCareApplication.class, args);
+    }
+}
+
+
+
